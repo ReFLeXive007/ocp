f072c687cf5c823e0fddd08e59fbd3bd39a1b5b0
ReFLeXive <reflexive007@yandex.ru>
2021-12-06 15:44:03 +0500

    заготовка для упражнений по книге Алгоритмы на джаве.

---
+++ algorithms/fundamentals/src/main/java/ru/reflexive/javabase/Ch1Exersises.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2021 Russian Post
+ *
+ * This source code is Russian Post Confidential Proprietary.
+ * This software is protected by copyright. All rights and titles are reserved.
+ * You shall not use, copy, distribute, modify, decompile, disassemble or reverse engineer the software.
+ * Otherwise this violation would be treated by law and would be subject to legal prosecution.
+ * Legal use of the software provides receipt of a license from the right holder only.
+ */
+
+package ru.reflexive.javabase;
+
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * @author Artur Gareev
+ */
+public class Ch1Exersises {
+    public static void main(String[] args) {
+
+    }
+}
